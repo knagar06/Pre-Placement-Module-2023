a8893db21c6a100a1eba5ba5159efc5e463024d5
kanishka <kanishkanagar222@gmail.com>
2022-07-09 18:34:34 +0530

    Create Squares of a Sorted Array.cpp

---
+++ ALGO I/Day 2/Squares of a Sorted Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]<0)
+                nums[i] = - nums[i];
+            else
+                break;       
+        }
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++)
+        {
+            nums[i] = nums[i]* nums[i];
+        }
+        return nums;
+    }
+};
